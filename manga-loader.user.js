9b8b82da470a75154932e97e0289b8fe991b4003
tv1mdb <11886891+tv1mdb@users.noreply.github.com>
2020-01-05 23:25:01 +0100

    Added mangadex.cc

--- manga-loader.user.js
+++ manga-loader.user.js
@@ -83,6 +83,7 @@
 // @match *://*.komikstation.com/*/*/*
 // @match *://*.gmanga.me/mangas/*/*/*
 // @match *://mangadex.org/chapter/*
+// @match *://mangadex.cc/chapter/*
 // @match *://merakiscans.com/*/*
 // @match *://biamamscans.com/read/*
 // @match *://read.lhtranslation.com/*.html
@@ -697,6 +698,31 @@ var implementations = [{
 }, {
   name: 'mangadex.org',
   match: "^https?://mangadex\\.org/chapter/[0-9]+/[0-9]+",
+  img: '#current_page',
+  next: function() {
+    return this._base + ++this._page;
+  },
+  numpages: '#jump_page',
+  curpage: '#jump_page',
+  nextchap: function() {
+    var chapter = document.querySelector('#jump_chapter').selectedOptions[0].previousElementSibling;
+     return (chapter === null) ? false : (this._base.replace(/[0-9]+\/$/, chapter.value));
+  },
+  prevchap: function() {
+    var chapter = document.querySelector('#jump_chapter').selectedOptions[0].nextElementSibling;
+     return (chapter === null) ? false : (this._base.replace(/[0-9]+\/$/, chapter.value));
+  },
+
+  wait: function() {
+    var loc = document.location.toString();
+    var num = loc.match(/[0-9]+$/);
+    this._base = loc.slice(0, -num.length);
+    this._page = parseInt(num);
+    return true;
+  }
+}, {
+  name: 'mangadex.cc',
+  match: "^https?://mangadex\\.cc/chapter/[0-9]+/[0-9]+",
   img: '#current_page',
   next: function() {
     return this._base + ++this._page;
