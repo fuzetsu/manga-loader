9843f546c1e10a81dd48a3d3b82173e271e9204e
Daniel Loomer <fuzetsu@users.noreply.github.com>
2020-11-29 22:21:01 -0500

    move type into interface

--- src/hooks/use-scroll-limit.ts
+++ src/hooks/use-scroll-limit.ts
@@ -1,17 +1,14 @@
 import { RefObject } from 'preact'
 import { useTimeout, useDOMEvent } from '.'
 
-export const useScrollLimit = ({
-  ref,
-  threshold = 3,
-  ontrigger,
-  disabled
-}: {
+interface Conf {
   ontrigger(): void
   disabled?: boolean
   threshold?: number
   ref?: RefObject<Element>
-}) => {
+}
+
+export const useScrollLimit = ({ ref, threshold = 3, ontrigger, disabled }: Conf) => {
   const check = () => {
     if (disabled) return
     const t = ref ? ref.current : document.scrollingElement
