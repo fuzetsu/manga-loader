62c508611e6276de7bb2b3a15401f65f10c37610
Daniel Loomer <fuzetsu@users.noreply.github.com>
2020-11-29 22:20:50 -0500

    properly inherit event type in handler

--- src/hooks/use-dom-event.ts
+++ src/hooks/use-dom-event.ts
@@ -3,30 +3,29 @@ import { useEffect } from 'preact/hooks'
 import { usePropRef } from '.'
 import { debounce, throttle } from '/lib/util'
 
-interface DOMEventConf {
-  type: string
-  handler(e: Event): void
-  disabled?: boolean
-  ref?: RefObject<Document | Element>
-  debounce?: number
-  throttle?: number
-}
-
 /** not passing ref will fallback on window */
-export const useDOMEvent = ({
+export const useDOMEvent = <T extends keyof WindowEventMap>({
   ref,
   type,
   handler,
   disabled,
   debounce: db,
   throttle: tt
-}: DOMEventConf) => {
+}: {
+  type: T
+  handler(evt: WindowEventMap[T]): void
+  disabled?: boolean
+  ref?: RefObject<Document | Element>
+  debounce?: number
+  throttle?: number
+}) => {
   const handlerRef = usePropRef(handler)
 
   // bind event using given options, restart if core configs change
   useEffect(() => {
     if (disabled) return
-    let eventHandler = (e: Event) => handlerRef.current(e)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let eventHandler = (e: any) => handlerRef.current(e)
     const id = { current: undefined }
     eventHandler = db
       ? debounce(db, eventHandler, id)
@@ -42,8 +41,8 @@ export const useDOMEvent = ({
   }, [ref, type, disabled, db, tt])
 }
 
-export const useGlobalEvent = (
-  type: DOMEventConf['type'],
-  handler: DOMEventConf['handler'],
-  opts: Omit<Omit<DOMEventConf, 'type'>, 'handler'>
+export const useGlobalEvent = <T extends keyof WindowEventMap>(
+  type: T,
+  handler: (e: WindowEventMap[T]) => void,
+  opts?: Omit<Omit<Omit<Parameters<typeof useDOMEvent>[0], 'type'>, 'handler'>, 'ref'>
 ) => useDOMEvent({ type, handler, ...opts })
